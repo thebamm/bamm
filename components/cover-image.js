101fee8da8b2d31b44917fd951021297bb6eb5af
object505 <pbeluent@gmail.com>
2020-12-14 09:42:02 +0100

    Updating image

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,14 +1,25 @@
 import cn from 'classnames'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function CoverImage({ title, src, slug, figcaption }) {
   const image = (
-    <img
+    // <img
+    //   src={src}
+    //   alt={`Cover Image for ${title}`}
+    //   className={cn('shadow-small', {
+    //     'hover:shadow-medium transition-shadow duration-200': slug,
+    //   })}
+    //   width={1600}
+    //   height={450}
+    // />
+    <Image
       src={src}
       alt={`Cover Image for ${title}`}
       className={cn('shadow-small', {
         'hover:shadow-medium transition-shadow duration-200': slug,
       })}
+      quality={100}
       width={1600}
       height={450}
     />
