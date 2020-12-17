a87076c0517814057989359cb3bcddb2e46d1990
object505 <pbeluent@gmail.com>
2020-12-17 23:49:55 +0100

    Adding webp handle

--- _posts/test-blog-posts.md
+++ _posts/test-blog-posts.md
@@ -1,14 +1,13 @@
 ---
 title: 'Test blog posts'
 excerpt: 'Testing blog features, new things test zone.'
-coverImage: 'https://unsplash.com/blog/content/images/2020/12/annie-spratt-T-Q2cBm0SIQ-unsplash--1-.jpg'
+coverImage: '/assets/blog/preview/cover.jpg'
 figcaption: 'Image from unsplash'
 date: '2020-12-12T05:35:07.322Z'
 author:
   name: Bojan Petkovski
-  picture: '/assets/blog/authors/jj.jpeg'
 ogImage:
-  url: 'https://unsplash.com/blog/content/images/2020/12/annie-spratt-T-Q2cBm0SIQ-unsplash--1-.jpg'
+  url: '/assets/blog/preview/cover.jpg'
 ---
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus. Praesent elementum facilisis leo vel fringilla. Congue mauris rhoncus aenean vel. Egestas sed tempus urna et pharetra pharetra massa massa ultricies.
--- components/cover-image.js
+++ components/cover-image.js
@@ -1,9 +1,28 @@
 import cn from 'classnames'
 import Link from 'next/link'
-// import Image from 'next/image'
+import Image from 'next/image'
 
 export default function CoverImage({ title, src, slug, figcaption, className }) {
   const imageClass = cn('bg-cover bg-center', className)
+  const pictureClass = cn('relative block overflow-hidden m-0 inset-0', className)
+
+  const pictureInlineClasses = {
+    visibility: 'visible',
+    position: 'absolute',
+    inset: 0,
+    boxSizing: 'border-box',
+    padding: 0,
+    border: 'none',
+    margin: 'auto',
+    display: 'block',
+    width: 0,
+    height: 0,
+    minWidth: '100%',
+    maxWidth: '100%',
+    minHeight: '100%',
+    maxHeight: '100%',
+    objectFit: 'cover',
+  }
 
   const image = (
     // <img
@@ -24,11 +43,18 @@ export default function CoverImage({ title, src, slug, figcaption, className }) 
     //   layout='fill'
     //   objectFit='cover'
     //   quality={100}
-    //   unoptimized={true}
+    //   // unoptimized={true}
     //   // width={1600}
     //   // height={450}
     // />
-    <div className={imageClass} style={{ backgroundImage: `url(${src})`}}/>
+    // <div className={imageClass} style={{ backgroundImage: `url(${src})`}}/>
+
+    <div className={pictureClass}>
+      <picture>
+          <source srcSet={`${src}.webp`} type="image/webp"/>
+          <img src={src} alt={title} style={pictureInlineClasses}/>
+      </picture>
+    </div>
   )
   return (
     <div className="sm:mx-0">
