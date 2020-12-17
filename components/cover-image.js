987966cdef9f96cbb8982563ca210b0f35360309
object505 <pbeluent@gmail.com>
2020-12-17 23:53:19 +0100

    Commented out next image optimization

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,6 +1,6 @@
 import cn from 'classnames'
 import Link from 'next/link'
-import Image from 'next/image'
+// import Image from 'next/image'
 
 export default function CoverImage({ title, src, slug, figcaption, className }) {
   const imageClass = cn('bg-cover bg-center', className)
